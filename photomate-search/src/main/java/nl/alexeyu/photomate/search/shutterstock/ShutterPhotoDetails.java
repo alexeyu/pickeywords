ae9b1c770ab5cfb364b665984ae5dc840008228c
Alexey Ustenko <alexeyu@gmail.com>
2019-11-30 21:01:26 +0100

    Don't forget to return Shutter's photo caption. Remove an unused method and a JSON example.

--- photomate-search/src/main/java/nl/alexeyu/photomate/search/shutterstock/ShutterPhotoDetails.java
+++ photomate-search/src/main/java/nl/alexeyu/photomate/search/shutterstock/ShutterPhotoDetails.java
@@ -8,66 +8,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import nl.alexeyu.photomate.model.PhotoMetaData;
 import nl.alexeyu.photomate.model.PhotoProperty;
 
-/*{
-   "illustration":1,
-   "r_rated":"0",
-   "photo_id":"119484406",
-   "enhanced_license_available":"1",
-   "resource_url":"http://api.shutterstock.com/images/119484406",
-   "categories":[
-      {
-         "category_id":"1",
-         "category":"Animals/Wildlife"
-      },
-      {
-         "category_id":"17",
-         "category":"Signs/Symbols"
-      }
-   ],
-   "model_release":{
-      "translation_id":"N_A",
-      "code":"0"
-   },
-   "vector_type":"eps",
-   "description":"Cats collection - vector silhouette",
-   "sizes":{
-      "small":{
-         "width":"500",
-         "height":"500",
-         "display_name":"Small"
-      },
-      "preview":{
-         "url":"http://thumb9.shutterstock.com/photos/display_pic_with_logo/856843/119484406.jpg"
-      },
-      "medium":{
-         "width":"1000",
-         "height":"999",
-         "display_name":"Medium"
-      },
-      "thumb_large":{
-         "width":"150",
-         "url":"http://thumb9.shutterstock.com/photos/thumb_large/856843/119484406.jpg",
-         "height":"150"
-      },
-      "thumb_small":{
-         "width":"100",
-         "url":"http://thumb9.shutterstock.com/photos/thumb_small/856843/119484406.jpg",
-         "img":"http://thumb9.shutterstock.com/photos/thumb_small/856843/119484406.jpg",
-         "height":"100"
-      },
-      "huge":{
-         "width":"800",
-         "height":"799",
-         "display_name":"Large"
-      }
-   },
-   "keywords":[],
-   "is_vector":1,
-   "web_url":"http://www.shutterstock.com/pic.mhtml?id=119484406",
-   "submitter_id":"856843",
-   "submitter":"Hein Nouwens"
-} 
-*/
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ShutterPhotoDetails implements PhotoMetaData {
 
@@ -96,6 +36,7 @@ public class ShutterPhotoDetails implements PhotoMetaData {
     public Object getProperty(PhotoProperty p) {
         switch (p) {
         case DESCRIPTION:
+        case CAPTION:
             return description();
         case KEYWORDS:
             return keywords();
--- photomate-search/src/main/java/nl/alexeyu/photomate/search/shutterstock/ShutterSearchResult.java
+++ photomate-search/src/main/java/nl/alexeyu/photomate/search/shutterstock/ShutterSearchResult.java
@@ -6,24 +6,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class ShutterSearchResult {
+final class ShutterSearchResult {
     
-    int count;
-    
-    List<ShutterPhotoDescription> photoDescriptions;
+    private List<ShutterPhotoDescription> photoDescriptions;
 
-    @JsonProperty("count")
-    public void setCount(int count) {
-        this.count = count;
-    }
-    
     @JsonProperty("results")
     public void setPhotoDescriptions(List<ShutterPhotoDescription> photoDescriptions) {
         this.photoDescriptions = photoDescriptions;
-    }
-
-    public int getCount() {
-        return count;
     }
 
     public List<ShutterPhotoDescription> getPhotoDescriptions() {
